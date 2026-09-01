6375ab1ebc05ea6da658ae8bf325b32b269787bf
figamore <90107339+figamore@users.noreply.github.com>
2026-09-01 12:44:03 -0400

    Bump version

--- src/lib/updateCheck.ts
+++ src/lib/updateCheck.ts
@@ -1,4 +1,4 @@
-export const CURRENT_VERSION = '1.2.9'
+export const CURRENT_VERSION = '1.3.0'
 export const GITHUB_REPO = 'figamore/FigUI'
 export const DISMISSED_VERSION_KEY = 'dismissed_update_version'
 
